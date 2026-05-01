208614c065dc1760a3965cc3110be4ab60662bfc
linecat <linecatstudio@outlook.com>
2026-05-02 01:16:37 +0800

    type(ControlX): Server/jest.config.js - add setupFiles to load setupEnv.ts

--- Server/jest.config.js
+++ Server/jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
   testTimeout: 15000,
+  setupFiles: ['<rootDir>/tests/setupEnv.ts'],
   // 移除被忽略的测试文件，让它们在Linux环境下运行
   // 这些测试需要WebSocket服务器，在Linux环境下可以正常运行
   testPathIgnorePatterns: []
